4b39e987065e56ceb85f50c09a275a01ed06ab9f
“wonjwi” <wonji616@gmail.com>
2021-07-26 13:53:51 +0900

    [#S05P12A607-146] build: front-end vue initializing

---
+++ frontend/vue.config.js
@@ -0,0 +1,30 @@
+// Vue3 관련 설정 파일
+module.exports = {
+  devServer: {
+    https: true,
+    port: 8083,
+    open: true,
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:8443/"
+      },
+      "/webjars": {
+        target: "http://localhost:8443/"
+      },
+      "/group-call": {
+        target: "http://localhost:8443/"
+      },
+      "/upload": {
+        target: "http://localhost:8443/"
+      }
+    },
+    historyApiFallback: true,
+    hot: true
+  },
+  css: {
+    requireModuleExtension: false // import 시에 경로에 .module 포함 안해도 됨.
+  },
+  transpileDependencies: ["element-plus"],
+  lintOnSave: false,
+  outputDir: "../backend/src/main/resources/dist"
+};
