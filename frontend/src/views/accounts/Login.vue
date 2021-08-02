892b6a9e6acadb9c152f6745ddbfc02cc09200a6
원지연 <yh3347@naver.com>
2021-08-03 07:08:04 +0900

    [#S05P12A607-43] design: 로그인 페이지 디자인

--- frontend/src/views/accounts/Login.vue
+++ frontend/src/views/accounts/Login.vue
@@ -2,135 +2,89 @@
   <div class="login">
     <div class="container">
       <div class="navbar">
-        <router-link to="/">
-          <img class="icon" src="@/assets/icons/arrow-left.png" alt="" />
-        </router-link>
+        <img
+          class="icon"
+          alt="arrow-left"
+          src="@/assets/icons/arrow-left.svg"
+          @click="$router.go(-1)"
+        />
       </div>
-      <div class="title">
-        <p class="main_title">로그인</p>
-        <p class="sub_title">비밀번호를 입력해주세요</p>
+      <h4 class="title">로그인</h4>
+      <p class="sub-title">이메일과 비밀번호를 입력해주세요</p>
+      <div class="input-div m-top-10">
+        <label for="email">이메일</label>
+        <div>
+          <input v-model="email" type="text" id="email" placeholder="booktory@example.com" />
+        </div>
       </div>
-      <div class="password_outer">
-        <input type="password" id="password" placeholder="비밀번호를 입력해주세요" />
+      <div class="input-div">
+        <label for="password">비밀번호</label>
+        <div>
+          <input
+            v-model="password"
+            type="password"
+            id="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
+        </div>
+        <p class="message">8자 이상 입력해주세요</p>
       </div>
-      <p class="message">8자 이상 입력해주세요</p>
-      <button type="button" class="btn">확인</button>
-      <router-link to="find">
-        <p class="find_password">비밀번호 찾기</p>
+      <button type="button" class="button-2 m-top-10" @click="clickLogin">확인</button>
+      <router-link to="password">
+        <p class="text-link">비밀번호 찾기</p>
       </router-link>
+      <div class="social-login">
+        <img src="@/assets/icons/naver.svg" />
+        <img src="@/assets/icons/kakao.svg" />
+      </div>
+      <div class="text-div">
+        <p class="font-body-3">아직 회원이 아니신가요?</p>
+        <h5 @click="$router.push({ name: 'Signup' })">회원가입하기</h5>
+      </div>
     </div>
   </div>
 </template>
 
+<script>
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    clickLogin() {
+      axios
+        .post("/users/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((res) => {
+          alert("로그인 성공");
+          console.log(res.data);
+        })
+        .catch((error) => {
+          alert("로그인 실패");
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
 <style scoped>
-.navbar {
-  text-align: left;
+.social-login img {
+  width: 4.8em;
+  height: 4.8em;
+  margin: 5% 1.5%;
 }
 
-.icon {
-  width: 24px;
-  height: 24px;
-  margin: 20px 0 0 20px;
-}
-
-.title {
-  width: 89.3%;
-  height: 59px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: stretch;
-  gap: 12px;
-  margin: 56px 5.3% 41px 5.3%;
-  padding: 0;
-}
-
-.main_title {
-  margin: 0;
-  height: 29px;
-  font-family: NotoSansKR;
-  font-size: 24px;
-  font-weight: bold;
-  line-height: 1.2;
-  text-align: center;
-  color: #212121;
-}
-
-.sub_title {
-  margin: 0;
-  height: 18px;
-  font-family: NotoSansKR;
-  font-size: 15px;
-  line-height: 1.2;
-  text-align: center;
-  color: #212121;
-}
-
-.password_outer {
-  display: inline-block;
-  text-align: left;
-  width: 84%;
-  height: 40px;
-  border-radius: 24px;
-  box-shadow: 0 4px 8px 0 rgba(142, 141, 208, 0.16);
-  background-color: #ffffff;
-}
-
-#password {
-  width: 88%;
-  padding: 13px 6% 12px;
-  border: 0;
-  background: transparent;
-}
-
-#password::placeholder {
-  font-family: NotoSansKR;
-  font-size: 15px;
-  line-height: 1.2;
-  color: #d6d6d9;
-}
-
-.message {
-  width: 25.6%;
-  height: 12px;
-  margin-left: 13.3%;
-  font-family: NotoSansKR;
-  font-size: 10px;
-  line-height: 1.2;
-  text-align: left;
-  color: #8a8894;
-}
-
-.btn {
-  width: 213px;
-  height: 48px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 200px auto 12px;
-  padding: 20px 32px;
-  border-radius: 50px;
-  border: 0;
-  background-color: #a4c0f3;
-  flex-grow: 0;
-  font-family: NotoSansKR;
-  font-size: 20px;
-  line-height: 1.2;
-  letter-spacing: 7px;
-  text-align: center;
-  color: #ffffff;
-}
-
-.find_password {
-  height: 18px;
-  font-family: NotoSansKR;
-  font-size: 15px;
-  line-height: 1.2;
-  text-align: center;
-  color: #8a8894;
-}
-
-a {
-  text-decoration: none;
+.text-div * {
+  color: var(--grey);
+  margin: 0.6em auto;
 }
 </style>
