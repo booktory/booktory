b3a1f9096baf575c354d6a0ff12ba567b25849bb
leeyeeun <maple1480@pusan.ac.kr>
2021-08-04 01:53:14 +0900

    [#S05P13A607-227] 파일명UUID생성

--- backend/src/main/java/com/ssafy/booktory/util/S3Uploader.java
+++ backend/src/main/java/com/ssafy/booktory/util/S3Uploader.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,7 +33,8 @@ public class S3Uploader implements Uploader{
     }
 
     private String upload(File uploadFile, String dirName){
-        String fileName = dirName + "/" + uploadFile.getName();
+        UUID uuid = UUID.randomUUID();
+        String fileName = dirName + "/booktory_" + uuid + "_" + uploadFile.getName();
         String uploadImageUrl = putS3(uploadFile, fileName);
         removeNewFile(uploadFile);
         return uploadImageUrl;
