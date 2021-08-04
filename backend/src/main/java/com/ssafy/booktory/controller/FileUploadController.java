25040abd16fee56d8249cb37a3205ee943257412
leeyeeun <maple1480@pusan.ac.kr>
2021-08-04 13:50:07 +0900

    [#S05P13A607-214] chore: 이미지 확장자 제한

--- backend/src/main/java/com/ssafy/booktory/controller/FileUploadController.java
+++ backend/src/main/java/com/ssafy/booktory/controller/FileUploadController.java
@@ -22,21 +22,26 @@ public class FileUploadController {
     private final Uploader uploader;
     private final Long FILE_LIMIT_SIZE = (long)1e7;
 
-    @PostMapping("/user")
+    @PostMapping( value = "/user", produces="application/json;charset=UTF-8")
     @ApiOperation(value = "프로필 이미지 등록", notes = "프로필 이미지를 S3원격 서버에 저장한다.")
     @ApiResponses({
             @ApiResponse(code = 201, message = "success"),
-            @ApiResponse(code = 400, message = "파일용량 초과"),
+            @ApiResponse(code = 400, message = "파일용량 초과 / 확장자 오류"),
             @ApiResponse(code = 401, message = "인증실패"),
             @ApiResponse(code = 404, message = "사용자 없음"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
-    public ResponseEntity<String> uploadProfileImg(@RequestParam("profileImg") MultipartFile file) throws IOException{
+    public ResponseEntity<String> uploadProfileImg(@RequestParam("file") MultipartFile file) throws IOException{
         log.info(String.format("업로드 파일 size : %d", file.getSize()));
         if(file.getSize() > FILE_LIMIT_SIZE){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("파일용량 초과");
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body(uploader.upload(file, "static/user"));
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(uploader.uploadS3Instance(file, "static/user"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @PostMapping("/board")
@@ -48,12 +53,12 @@ public class FileUploadController {
             @ApiResponse(code = 404, message = "사용자 없음"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
-    public ResponseEntity<String> uploadBoardFile(@RequestParam("profileImg") MultipartFile file) throws IOException{
+    public ResponseEntity<String> uploadBoardFile(@RequestParam("file") MultipartFile file) throws Exception {
         log.info(String.format("업로드 파일 size : %d", file.getSize()));
         if(file.getSize() > FILE_LIMIT_SIZE){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("파일용량 초과");
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body(uploader.upload(file, "static/board"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(uploader.uploadS3Instance(file, "static/board"));
     }
 
 
--- backend/src/main/java/com/ssafy/booktory/util/S3Uploader.java
+++ backend/src/main/java/com/ssafy/booktory/util/S3Uploader.java
@@ -14,7 +14,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -24,22 +23,27 @@ import java.util.UUID;
 @Component
 public class S3Uploader implements Uploader{
 
-    private final static String TEMP_FILE_PATH = "src/main/resources/";
+    private final static String TEMP_FILE_PATH = "src/main/resources/static/";
     private final AmazonS3Client amazonS3Client;
-    private final List<String> imageExt= Arrays.asList(".PNG", ".png", ".JPEG", ".jpeg", ".JPG", ".jpg", ".TIFF", ".tiff", ".BMP", ".bmp" , ".GIF", ".gif");
+    private final List<String> imageExt = Arrays.asList(".PNG", ".png", ".JPEG", ".jpeg", ".JPG", ".jpg", ".TIFF", ".tiff", ".BMP", ".bmp" , ".GIF", ".gif");
 
     @Value("${cloud.aws.s3.bucket}")
     public String bucket;
 
     @Override
-    public String upload(MultipartFile multipartFile, String dirName) throws Exception {
+    public String uploadS3Instance(MultipartFile multipartFile, String dirName) throws IOException {
         File convertedFile = convert(multipartFile);
-        if(dirName == "static/user"){
-            String ext = getExt(convertedFile.getName());
-            if(!imageExt.contains(ext))
-                throw new UnsupportedOperationException("지원하지 않는 확장자입니다.");
+        if(dirName == "static/user" && !imageExt.contains(getExt(convertedFile.getName()))) {
+            removeNewFile(convertedFile);
+            throw new UnsupportedOperationException("지원하지않는 확장자 입니다.");
         }
         return upload(convertedFile, dirName);
+    }
+
+    @Override
+    public void deleteS3Instance(String fileName) {
+        amazonS3Client.deleteObject(bucket, fileName);
+        log.info(fileName + " 삭제완료");
     }
 
     private String upload(File uploadFile, String dirName){
