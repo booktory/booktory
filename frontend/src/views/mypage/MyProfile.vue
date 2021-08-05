c58fbfd79b6d498d1f1d009315917affc74e9404
원지연 <yh3347@naver.com>
2021-08-06 05:11:28 +0900

    [#S05P13A607-177] feat: 회원 프로필 정보를 보여주는 MyPage 추가

---
+++ frontend/src/views/mypage/MyProfile.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <h4 class="title">나의 프로필</h4>
+      <div class="icon settings-btn" @click="$router.push({ name: 'AccountManage' })">
+        <icon-base><icon-settings /></icon-base>
+      </div>
+    </div>
+    <div class="user-info">
+      <img class="profile-image" src="@/assets/images/profile_default.svg" alt="profile-image" />
+      <div class="info-frame">
+        <span class="font-body-4">{{ mainBadge }}</span>
+        <h5>{{ nickname }}</h5>
+        <span class="font-body-4">{{ email }}</span>
+      </div>
+    </div>
+    <button type="button" class="button-1 m-top-5" @click="$router.push({ name: 'UpdateProfile' })">
+      프로필 수정
+    </button>
+  </div>
+</template>
+
+<script>
+import IconSettings from "@/components/icons/IconSettings.vue";
+
+export default {
+  name: "MyProfile",
+  components: {
+    IconSettings,
+  },
+  props: {
+    mainBadge: {
+      type: String,
+    },
+    nickname: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  position: relative;
+}
+.settings-btn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-right: 8%;
+}
+.user-info {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 4.8%;
+  margin: 6.4% 8.2% 0;
+  padding: 0 2%;
+}
+.profile-image {
+  width: 6em;
+  height: 6em;
+  border-radius: 10em;
+}
+.info-frame {
+  display: flex;
+  flex-direction: column;
+}
+.info-frame * {
+  text-align: left;
+  margin: 1% 0;
+}
+</style>
