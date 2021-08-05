0b21e0408e31949b0d2f7bec09d519b083a4ba77
원지연 <yh3347@naver.com>
2021-08-06 00:28:57 +0900

    [#S05P13A607-36] feat: SVG 아이콘 컴포넌트명 정리 및 이로 인한 코드 수정

--- frontend/src/views/accounts/Register.vue
+++ frontend/src/views/accounts/Register.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="navbar">
       <div class="icon" @click="$router.go(-1)">
-        <icon-base><arrow-left /></icon-base>
+        <icon-base><icon-arrow-left /></icon-base>
       </div>
     </div>
     <h4 class="title">회원가입</h4>
@@ -64,11 +64,8 @@ import router from "@/router";
 // import axios from "axios";
 // import Swal from "sweetalert2";
 
-import IconBase from "../../components/icons/IconBase.vue";
-import ArrowLeft from "../../components/icons/ArrowLeft.vue";
-
 export default {
-  components: { IconBase, ArrowLeft },
+  components: {},
   name: "Register",
   data() {
     return {
