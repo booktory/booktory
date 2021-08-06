79ff46bcf2175cea07b3d04fd5b3b17f635ca127
leeyeeun <maple1480@pusan.ac.kr>
2021-08-06 15:16:21 +0900

    [#S05P13A607-87] fix: modifiedDate 자동생성에 따른 쿼리수정

--- backend/src/main/java/com/ssafy/booktory/domain/rwmlog/RwmLogRepository.java
+++ backend/src/main/java/com/ssafy/booktory/domain/rwmlog/RwmLogRepository.java
@@ -2,10 +2,16 @@ package com.ssafy.booktory.domain.rwmlog;
 
 import com.ssafy.booktory.domain.rwm.Rwm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface RwmLogRepository extends JpaRepository<RwmLog, Long> {
-    int countByRwmAndModifiedDateIsNull(Rwm rwm);
-    List<RwmLog> findAllByRwmAndModifiedDateIsNull(Rwm rwm);
+
+    @Query("select rl from RwmLog rl where rl.rwm = :rwm and rl.createdDate = rl.modifiedDate")
+    List<RwmLog> findAllByRwmAndDateTime(@Param("rwm") Rwm rwm);
+    @Query("select count(rl) from RwmLog rl where rl.rwm = :rwm and rl.createdDate = rl.modifiedDate")
+    int countByRwmAndDateTime(Rwm rwm);
 }
