8542c1d342f38c97f68099355639b83a7106105f
원지연 <yh3347@naver.com>
2021-08-09 03:00:24 +0900

    [#S05P13A607-35] feat: 회원가입 추가정보 입력 값 유효성 검사

--- frontend/src/store/modules/accountStore.js
+++ frontend/src/store/modules/accountStore.js
@@ -1,6 +1,6 @@
 import SERVER from "@/api/api";
 import axios from "axios";
-// import router from "@/router";
+import router from "@/router";
 import Swal from "sweetalert2";
 
 const accountStore = {
@@ -13,8 +13,8 @@ const accountStore = {
       let timerInterval;
       Swal.fire({
         title: "인증 메일 발송 중",
-        html: "조금만 기다려주세요.",
-        timer: 4000,
+        html: "잠시만 기다려주세요.",
+        timer: 2000,
         timerProgressBar: true,
         onBeforeOpen: () => {
           Swal.showLoading();
@@ -57,6 +57,29 @@ const accountStore = {
         location: SERVER.ROUTES.register,
       };
       dispatch("postAuthData", info);
+    },
+    // 회원가입 - 추가정보 입력
+    registerExtraInfo({ dispatch }, extraData) {
+      console.log(dispatch);
+      axios
+        .patch(SERVER.URL + SERVER.ROUTES.registerExtraInfo, extraData)
+        .then((res) => {
+          Swal.fire({
+            icon: "success",
+            title: "회원가입 완료",
+            html: "회원가입이 완료 되었습니다.<br>지금부터 책토리를 이용하실 수 있어요!",
+          });
+          console.log(res.data);
+          router.push({ name: "Login" });
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "추가정보 입력 실패",
+            text: "추가정보 입력에 실패했습니다.",
+          });
+          console.log(error);
+        });
     },
   },
 };
--- frontend/src/views/accounts/ExtraInfo.vue
+++ frontend/src/views/accounts/ExtraInfo.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="container m-top">
     <h4 class="title">추가정보 입력</h4>
-    <p class="sub-title">프로필을 입력해주세요</p>
+    <p class="sub-title">추가정보를 입력해주세요</p>
     <div class="m-top-10">
       <div class="profile-img-div">
         <input
           class="hidden-item"
           ref="image"
+          id="profileImg"
           type="file"
           accept="image/*"
           @change="uploadImage()"
         />
-        <div class="icon profile-btn" @click="clickProfile">
+        <div class="icon profile-btn" @click="clickProfileImg">
           <icon-base><icon-camera /></icon-base>
         </div>
         <img class="profile-img" src="@/assets/images/profile_default.svg" alt="Profile Image" />
@@ -21,88 +22,104 @@
       <label for="name">이름</label>
       <div>
         <input
-          v-model="extrainfoData.name"
+          v-model="extraData.name"
+          v-bind:class="{
+            error: error.name,
+            complete: !error.name && extraData.name.length !== 0,
+          }"
           type="text"
           id="name"
+          maxlength="20"
           placeholder="이름을 입력해주세요"
+          autocapitalize="none"
+          autocorrect="none"
+          required
         />
       </div>
+      <p v-if="error.name" class="message">{{ error.name }}</p>
     </div>
     <div class="input-div">
       <label for="birth">생년월일</label>
       <date-picker
         id="birth"
-        v-model="extrainfoData.birth"
+        v-model="extraData.birth"
+        v-bind:class="{
+          error: error.birth,
+          complete: !error.birth && extraData.birth.length !== 0,
+        }"
         format="YYYY-MM-DD"
         type="date"
-        placeholder="YYYY-MM-DD"
+        placeholder="생년월일을 선택해주세요"
         :default-value="defaultDate"
         :disabled-date="disabledAfterTodayAndBefore100Year"
         :clearable="false"
+        required
       ></date-picker>
-      <!-- </div> -->
-      <p class="message">생년월일을 선택해주세요</p>
+      <p v-if="error.birth" class="message">{{ error.birth }}</p>
     </div>
     <div class="input-div">
       <label for="phone">전화번호</label>
       <div>
-        <input v-model="extrainfoData.phone" type="tel" id="phone" placeholder="010-0000-0000" />
+        <input
+          v-model="extraData.phone"
+          v-bind:class="{
+            error: error.phone,
+            complete: !error.phone && extraData.phone.length !== 0,
+          }"
+          type="tel"
+          id="phone"
+          maxlength="11"
+          placeholder="전화번호를 입력해주세요"
+          required
+        />
       </div>
-      <p class="message">전화번호를 입력해주세요</p>
+      <p v-if="error.phone" class="message">{{ error.phone }}</p>
     </div>
     <button type="button" class="button-2 white-bg m-top-10" @click="clickSkip">넘어가기</button>
-    <button type="button" class="button-2" @click="clickExtraInfo">확인</button>
+    <button type="button" class="button-2" @click="clickExtraInfo">추가정보 입력</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import router from "@/router";
 import axios from "axios";
 import Swal from "sweetalert2";
 import IconCamera from "@/components/icons/IconCamera.vue";
 
 export default {
-  name: "Register",
+  name: "ExtraInfo",
   components: { IconCamera },
   data() {
     return {
-      extrainfoData: {
-        profile_img: "",
+      extraData: {
+        email: "wonji616@gmail.com",
+        profileImg: "",
         name: "",
         birth: "",
         phone: "",
       },
       defaultDate: new Date(new Date().getTime() - 20 * 365 * 24 * 3600 * 1000),
+      error: {
+        name: false,
+        birth: false,
+        phone: false,
+      },
     };
   },
-  computed: {},
+  watch: {
+    extraData: {
+      deep: true,
+      handler() {
+        this.checkNameForm();
+        this.checkBirthForm();
+        this.checkPhoneForm();
+      },
+    },
+  },
   methods: {
-    clickProfile() {
-      this.$refs["image"].click();
-    },
-    uploadImage() {
-      let form = new FormData();
-      let image = this.$refs["image"].files[0];
-
-      form.append("image", image);
-
-      console.log(image);
-
-      // axios
-      //   .post("/upload", form, {
-      //     header: { "Content-Type": "multipart/form-data" },
-      //   })
-      //   .then(({ data }) => {
-      //     this.images = data;
-      //   })
-      //   .catch((err) => console.log(err));
-    },
-    disabledAfterTodayAndBefore100Year(date) {
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-
-      return date > today || date < new Date(today.getTime() - 100 * 365 * 24 * 3600 * 1000);
-    },
+    ...mapActions("accountStore", ["registerExtraInfo"]),
+    // 넘어가기 버튼 클릭
     clickSkip() {
       Swal.fire({
         icon: "success",
@@ -111,31 +128,77 @@ export default {
       });
       router.push({ name: "Login" });
     },
+    // 추가정보 입력 버튼 클릭
     clickExtraInfo() {
+      // 유효성 검사 통과 시 작동
+      let isSubmit = true;
+      Object.values(this.error).map((v) => {
+        if (v) isSubmit = false;
+      });
+      if (isSubmit) {
+        this.registerExtraInfo(this.extraData);
+      }
+    },
+    // 프로필 사진 버튼 클릭
+    clickProfileImg() {
+      this.$refs["image"].click();
+    },
+    // 프로필 사진 업로드
+    uploadImage() {
+      let form = new FormData();
+      let image = this.$refs["image"].files[0];
+      form.append("file", image);
       axios
-        .post("/users/extra", {
-          profile_img: this.extrainfoData.profile_img,
-          name: this.extrainfoData.nickname,
-          birth: this.extrainfoData.email,
-          phone: this.extrainfoData.password,
+        .post("/api/files/user", form, {
+          headers: { "Content-Type": "multipart/form-data" },
         })
-        .then((res) => {
-          Swal.fire({
-            icon: "success",
-            title: "회원가입 완료",
-            html: "회원가입이 완료 되었습니다.<br>지금부터 책토리를 이용하실 수 있어요!",
-          });
-          console.log(res.data);
-          router.push({ name: "Login" });
+        .then(({ data }) => {
+          this.extraData.profileImg = data;
+          console.log(this.extraData.profileImg);
         })
-        .catch((error) => {
-          Swal.fire({
-            icon: "error",
-            title: "추가정보 입력 실패",
-            text: "추가정보 입력에 실패했습니다.",
-          });
-          console.log(error);
-        });
+        .catch((err) => console.log(err));
+    },
+    // 생년월일 입력 제한
+    disabledAfterTodayAndBefore100Year(date) {
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      return date > today || date < new Date(today.getTime() - 100 * 365 * 24 * 3600 * 1000);
+    },
+    checkNameForm() {
+      if (this.extraData.name.length > 0 && this.extraData.name.length > 20) {
+        this.error.name = "이름이 너무 길어요! 20자 이하로 입력해주세요";
+      } else if (this.extraData.name.length >= 1 && !this.validName(this.extraData.name)) {
+        this.error.name = "이름은 한글과 영문으로만 입력 가능 해요";
+      } else this.error.name = false;
+    },
+    validName(name) {
+      // eslint-disable-next-line
+      var reg = /^[가-힣a-zA-Z\s]+$/;
+      return reg.test(name);
+    },
+    checkBirthForm() {
+      if (this.extraData.birth.length > 0 && !this.validBirth(this.extraData.birth)) {
+        this.error.birth = "생년월일을 올바른 형식으로 입력해주세요";
+      } else this.error.birth = false;
+    },
+    validBirth(birth) {
+      // eslint-disable-next-line
+      var reg = /^\d{4}-\d{2}-\d{2}$/;
+      return reg.test(birth);
+    },
+    checkPhoneForm() {
+      if (this.extraData.phone.length > 0 && this.extraData.phone.length < 9) {
+        this.error.phone = "전화번호가 너무 짧아요";
+      } else if (this.extraData.phone.length > 11) {
+        this.error.phone = "전화번호가 너무 길어요";
+      } else if (this.extraData.phone.length >= 9 && !this.validPhone(this.extraData.phone)) {
+        this.error.phone = "전화번호를 숫자로만 입력해주세요";
+      } else this.error.phone = false;
+    },
+    validPhone(phone) {
+      // eslint-disable-next-line
+      var reg = /^[0-9]*$/;
+      return reg.test(phone);
     },
   },
   mounted() {},
