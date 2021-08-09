379843f841c7d86a653abdae3e6ffd6a294efb43
원지연 <yh3347@naver.com>
2021-08-09 16:21:04 +0900

    [#S05P13A607-17] fix: 로그인 처리 내용 수정

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,27 +1,36 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+Vue.use(Vuex);
+
 import examples from "./modules/examples.js";
 import accountStore from "@/store/modules/accountStore";
+import mypageStore from "@/store/modules/mypageStore";
 
 import cookies from "vue-cookies";
-
-Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     authToken: cookies.get("auth-token"),
+    user: localStorage.getItem("user"),
   },
-  getters: {},
+  getters: {
+    config: (state) => ({ headers: { jwt: state.authToken } }),
+  },
   mutations: {
-    SET_TOKEN(state, token) {
-      state.authToken = token;
-      cookies.set("auth-token", token);
+    SET_TOKEN(state, data) {
+      state.authToken = data;
+      cookies.set("auth-token", data);
+    },
+    SET_USER(state, data) {
+      state.user = data;
+      localStorage.setItem("user", data);
     },
   },
   actions: {},
   modules: {
     accountStore,
+    mypageStore,
     examples,
   },
 });
