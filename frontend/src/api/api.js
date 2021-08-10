257f0e0eaebe2a86335f4094711805c50e8eb9e7
Wonsun Yoo <heidencena1@gmail.com>
2021-08-10 23:23:45 +0900

    [#S05P13A607-265] feat: 알람 읽음 처리

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
 export default {
-  URL: "/api",
+  URL: "http://localhost:8080/api",
   ROUTES: {
     // accounts
     login: "/users/login",
--- frontend/src/views/TopHeader.vue
+++ frontend/src/views/TopHeader.vue
@@ -6,21 +6,53 @@
     <span class="font-body-4 welcome">안녕하세요, {{ user.nickname }}님</span>
     <div class="icon right" @click="$router.push({ name: 'Notification' })">
       <icon-base :width="'1.6em'" :height="'1.6em'"><icon-bell /></icon-base>
+      <div v-if="!this.isRead" class="noti"></div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import fire from "@/firebase.js";
 
 export default {
   components: {},
+  data() {
+    return {
+      isRead: true,
+    };
+  },
   name: "Navbar",
   computed: {
     ...mapState(["user"]),
   },
-  methods: {},
-  created() {},
+  methods: {
+    pollAlarm() {
+      let tries = 10;
+      let polling = setInterval(() => {
+        if (!tries--) {
+          console.log(tries);
+          clearInterval(polling);
+        } else {
+          let chk = true;
+          const usersref = fire.database().ref(`users/${this.user.nickname}`).limitToLast(1);
+          usersref.on("value", (list) => {
+            const data = list.val();
+            for (let key in data) {
+              if (data[key].readStatus == 0) {
+                console.log(data[key].readStatus);
+                chk = false;
+              }
+            }
+          });
+          this.isRead = chk;
+        }
+      }, 500);
+    },
+  },
+  created() {
+    this.pollAlarm();
+  },
 };
 </script>
 
@@ -29,8 +61,18 @@ export default {
   justify-self: left;
   align-items: center;
   display: flex;
+  position: relative;
 }
 .welcome {
   padding-right: 0.4em;
+}
+.noti {
+  width: 0.4em;
+  height: 0.4em;
+  background-color: var(--orange);
+  border-radius: 50%;
+  position: absolute;
+  left: 1.6em;
+  bottom: 1.6em;
 }
 </style>
