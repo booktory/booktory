a2976900deb3dbc21e310c8e5865589f8ed3aee4
won <hksoftcorn.dev@gmail.com>
2021-08-11 18:09:27 +0900

    [#S05P13A607-33] design: 클럽 상세 design 작업

--- frontend/src/views/clubdetail/ClubdetailMeeting/ClubdetailMeetingCreate.vue
+++ frontend/src/views/clubdetail/ClubdetailMeeting/ClubdetailMeetingCreate.vue
@@ -9,24 +9,45 @@
           <div class="main-head">
             <h4 class="title">모임 생성</h4>
           </div>
-          <div class="create-meeting-card">
+          <div class="create-meeting-card m-top-1">
             <div class="create-meeting-schedule">
               <div class="create-meeting-schedule-head">
-                <h5>모임 일정</h5>
+                <h5>모임 날짜</h5>
               </div>
-              <div class="create-meeting-schedule-body">
-                <date-picker></date-picker>
+              <div class="create-meeting-schedule-body m-top-1">
+                <date-picker
+                  v-model="value1"
+                  type="date"
+                  placeholder="날짜를 선택하세요"
+                  class="date-picker"
+                  required
+                ></date-picker>
+              </div>
+              <div class="create-meeting-schedule-head m-top-2">
+                <h5>모임 시간</h5>
+              </div>
+              <div class="create-meeting-schedule-body m-top-1">
+                <date-picker
+                  v-model="value2"
+                  type="time"
+                  placeholder="시간을 선택하세요"
+                  class="date-picker"
+                  required
+                ></date-picker>
               </div>
             </div>
             <div class="create-meeting-book">
-              <div class="create-meeting-book-head">
+              <div class="create-meeting-book-head m-top-2">
                 <h5>읽을 책 선택하기</h5>
               </div>
-              <div class="create-meeting-book-body">
-                <select name="" id=""></select>
+              <div class="create-meeting-book-body m-top-1">
+                <select name="" id="">
+                  <option>1</option>
+                  <option>2</option>
+                </select>
               </div>
             </div>
-            <button class="button-2">만들기</button>
+            <button class="button-2 adj-center">만들기</button>
           </div>
         </div>
       </div>
@@ -43,9 +64,26 @@ export default {
   components: {
     Navbar,
   },
+  data() {
+    return {
+      value1: null,
+      value2: null,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "./ClubdetailMeetingCreate.scss";
+.m-top-1 {
+  margin-top: 1rem;
+}
+
+.m-top-2 {
+  margin-top: 2rem;
+}
+
+.adj-center {
+  margin: 0 auto;
+}
 </style>
