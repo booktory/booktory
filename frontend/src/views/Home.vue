85f4f6616514d3c7606a9440779691ec1f482824
원지연 <yh3347@naver.com>
2021-08-12 23:48:54 +0900

    [#S05P13A607-36] design: 랜딩 페이지 이미지 교체

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="container m-top">
     <h4 class="title">책토리와 함께하는 <span class="title-orange">독서모임</span></h4>
     <p class="sub-title">책을 읽고 사람들과 함께 생각을 공유해요</p>
-    <img class="logo" src="@/assets/images/booktory.svg" alt="Booktory Logo" />
+    <img class="logo" src="@/assets/images/landing_page.png" alt="Booktory" />
     <button type="button" class="button-2" @click="$router.push({ name: 'Login' })">
       시작하기
     </button>
@@ -26,8 +26,8 @@ export default {
 }
 
 .logo {
-  width: 88%;
-  margin: 1rem 0 0;
+  width: 80%;
+  margin: 4rem 0 0;
   object-fit: contain;
 }
 </style>
