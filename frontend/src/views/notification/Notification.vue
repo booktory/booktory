096cc7185bad5f1874a6440133d479b97ba1cb1b
원지연 <yh3347@naver.com>
2021-08-18 03:35:03 +0900

    [#S05P13A607-300] design: 알림 페이지 디자인 수정

--- frontend/src/views/notification/Notification.vue
+++ frontend/src/views/notification/Notification.vue
@@ -4,32 +4,24 @@
       <TopHeader />
       <h4 class="title">알림</h4>
       <div class="alarm-wrapper">
-        <div class="alarm-list">
-          <div v-if="alarmList.length != 0">
-            <div class="" v-for="(alarm, idx) in alarmList" :key="idx">
-              <div class="alarm-contents unread" v-if="alarm.status == 0">
-                <div v-html="alarm.message" class="alarm-contents-message"></div>
-                <div class="alarm-contents-time">{{ convertTime(alarm.time) }}</div>
-              </div>
-              <div class="alarm-contents" v-else>
-                <div v-html="alarm.message" class="alarm-contents-message"></div>
-                <div class="alarm-contents-time">{{ convertTime(alarm.time) }}</div>
-              </div>
+        <div v-if="alarmList.length > 0" class="alarm-list">
+          <div v-for="(alarm, idx) in alarmList" :key="idx">
+            <div class="alarm-contents" :class="{ unread: alarm.status == 0 }">
+              <div class="alarm-contents-message" v-html="alarm.message"></div>
+              <div class="alarm-contents-time">{{ convertTime(alarm.time) }}</div>
             </div>
           </div>
-          <div v-else>
-            <div class="alarm-contents-no">
-              <p>받은 알림이 없습니다.</p>
-            </div>
-          </div>
-          <div v-if="alarmList.length < page * 5 && alarmList.length > 0">
-            <p class="alarm-button-no">더 이상 알림이 존재하지 않습니다.</p>
-          </div>
-          <div v-else-if="alarmList.length >= page * 5">
-            <button type="button" class="alarm-button p-b-1" @click="getMoreAlarm">
+          <div v-if="alarmList.length >= page * 7">
+            <button type="button" class="alarm-button" @click="getMoreAlarm">
               더 많은 알림 보기
             </button>
           </div>
+          <div v-else>
+            <p class="alarm-button-no">더 이상 알림이 존재하지 않습니다</p>
+          </div>
+        </div>
+        <div v-else class="alarm-contents-no">
+          <p>받은 알림이 없습니다.</p>
         </div>
       </div>
     </div>
@@ -57,7 +49,7 @@ export default {
   },
   created() {
     this.alarmList = [];
-    const usersref = fire.database().ref(`users/${this.userId}`).limitToLast(5);
+    const usersref = fire.database().ref(`users/${this.userId}`).limitToLast(7);
     usersref.on("value", (list) => {
       const data = list.val();
       for (let key in data) {
@@ -91,14 +83,14 @@ export default {
       const usersref = fire
         .database()
         .ref(`users/${this.userId}`)
-        .limitToLast(5 * this.page);
+        .limitToLast(7 * this.page);
 
       usersref.on("value", (list) => {
         const data = list.val();
         let arrData = Object.entries(data).reverse();
         var idx = 1;
         for (let i = 1; i <= arrData.length; i++) {
-          if (idx++ > prev * 5) {
+          if (idx++ > prev * 7) {
             this.alarmList.push({
               message: arrData[i - 1][1].message,
               time: arrData[i - 1][1].createDateTime,
@@ -129,59 +121,54 @@ export default {
 </script>
 <style scoped>
 .alarm-wrapper {
-  margin: 5% 7%;
-  width: 86%;
+  width: 32rem;
+  margin: 2rem auto;
+  padding: 0;
+  overflow: hidden;
   border-radius: 1rem;
-  box-shadow: 0 0.4em 0.8em 0 rgb(142 141 208 / 16%);
+  box-shadow: 0 0.4rem 0.8rem 0.4rem rgb(161, 160, 228, 0.16);
   background-color: var(--white);
 }
-
 .alarm-list {
-  padding: 0.5rem 0.3rem 0;
+  display: flex;
+  flex-direction: column;
 }
-
 .alarm-contents {
-  padding: 4% 3%;
+  padding: 1.5rem 1.8rem 1rem;
   border-bottom: 1px solid var(--very-light-grey);
 }
-
-.alarm-contents-no {
-  line-height: 1.2;
+.alarm-contents-no p {
+  margin: 2rem auto;
+  font-size: 1.2rem;
 }
-
 .alarm-contents-message {
   text-align: left;
+  font-size: 1.1rem;
 }
-
 .alarm-contents-time {
-  font-size: 0.7rem;
   margin-top: 1rem;
   text-align: right;
+  color: var(--grey);
 }
-
 .unread {
   background-color: var(--very-light-orange);
 }
-
 .alarm-button {
-  font-size: 1rem;
+  font-size: 1.1rem;
   letter-spacing: 0.2rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 5% auto;
-  padding: 2.5% 4%;
-  border-radius: 3em;
+  margin: 1.5rem auto;
+  padding: 0.8rem 1.8rem;
+  border-radius: 2em;
   border: 0;
   background-color: var(--orange);
   color: var(--white);
 }
-
 .alarm-button-no {
-  padding: 2.5% 4%;
-}
-
-.p-b-1 {
-  padding-bottom: 1rem;
+  margin: 2.4rem auto;
+  font-size: 1.1rem;
+  color: var(--orange);
 }
 </style>
