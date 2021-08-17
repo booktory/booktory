a9cc7f5912fe24001e5fa94626fa5e6194031a9c
won <hksoftcorn.dev@gmail.com>
2021-08-17 11:32:19 +0900

    [#S05P13A607-294] feat: 메인페이지 애니메이션 추가

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,7 +3,8 @@
     <div>
       <h4 class="title">책토리와 함께하는 <span class="title-orange">독서모임</span></h4>
       <p class="sub-title">책을 읽고 사람들과 함께 생각을 공유해요</p>
-      <img class="logo" src="@/assets/images/landing_page.png" alt="Booktory" />
+      <div class="landing"></div>
+      <!-- <img class="logo" src="@/assets/images/landing_page.png" alt="Booktory" /> -->
       <button type="button" class="button-2" @click="$router.push({ name: 'Login' })">
         시작하기
       </button>
@@ -29,4 +30,20 @@ export default {
   margin: 3rem 0 1rem;
   object-fit: contain;
 }
+
+@keyframes landing-ani {
+  to {
+    background-position: -225rem 0;
+  }
+}
+
+.landing {
+  width: 37.5rem;
+  height: 37.5rem;
+  margin: 1rem auto;
+  object-fit: contain;
+  background: url("../assets/images/landing.png") no-repeat 0 0 / auto 37.5rem;
+  animation: landing-ani 2.5s infinite steps(6);
+}
+/* reverse */
 </style>
