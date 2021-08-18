d9182fc28b646978633ef9f59e7128511c8b8dc1
원지연 <yh3347@naver.com>
2021-08-18 11:25:12 +0900

    [#S05P13A607-313] design: RWM 페이지 css 수정

--- frontend/src/views/rwm/RwmMainCardsDetail.vue
+++ frontend/src/views/rwm/RwmMainCardsDetail.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="rwm-cards-detail" @click="clickRwmCard">
     <div class="rwm-img-wrapper">
-       <img :src="img" :alt="img" class="rwm-img" />
+      <img :src="img" :alt="img" class="rwm-img" />
     </div>
-    <h5 class="">{{name}}</h5>
-    <p class="">{{userCount}}명이 참여하고 있어요</p>
-    <p class="">{{info}}</p>
+    <h5 class="rwm-title">{{ name }}</h5>
+    <p class="rwm-user">{{ userCount }}명이 참여하고 있어요</p>
+    <p class="rwm-info">{{ info }}</p>
 
     <div v-if="index % 2 == 0" class=""></div>
-
-
   </div>
 </template>
 
 <script>
-
 import router from "@/router";
 
 export default {
   name: "RwmMainCardsDetail",
   components: {},
-  props:{
+  props: {
     index: {
       type: Number,
     },
@@ -38,26 +35,36 @@ export default {
     },
     userCount: {
       type: Number,
-    }
+    },
   },
   methods: {
-    clickRwmCard(){
-      router.push({ name: 'RwmEnterance', query: {id: this.$props.id} })
-    }
+    clickRwmCard() {
+      router.push({ name: "RwmEnterance", query: { id: this.$props.id } });
+    },
   },
 };
 </script>
 
 <style scoped>
 .rwm-cards-detail {
-  width: 40vw;
-  height: auto;
-  border-radius: 5px;
-  box-shadow: 0 4px 8px 4px rgba(161, 160, 228, 0.16);
+  border-radius: 1rem;
+  box-shadow: 0 0.2rem 0.6rem 0.2rem rgba(161, 160, 228, 0.16);
   background-color: var(--white);
+  padding: 0.7rem 0.7rem 1rem;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
 }
-.rwm-img{
-    width: 100%;
+.rwm-cards-detail * {
+  margin: 0;
 }
-
+.rwm-img {
+  border-radius: 0.7rem;
+  width: 100%;
+  height: auto;
+}
+.rwm-user {
+  color: var(--grey);
+}
 </style>
