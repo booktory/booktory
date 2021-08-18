9324518a8dbcc5ffe3dfc019d9e5553da5dd139a
Wonsun Yoo <heidencena1@gmail.com>
2021-08-18 16:14:39 +0900

    [#S05P13A607-317] feat: 현재 모임 참여 인원 api

--- backend/src/main/java/com/ssafy/booktory/domain/bookclubuser/BookClubUser.java
+++ backend/src/main/java/com/ssafy/booktory/domain/bookclubuser/BookClubUser.java
@@ -2,10 +2,12 @@ package com.ssafy.booktory.domain.bookclubuser;
 
 import com.ssafy.booktory.domain.bookclub.BookClub;
 import com.ssafy.booktory.domain.user.User;
+import com.sun.istack.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
@@ -26,10 +28,17 @@ public class BookClubUser {
     @JoinColumn(name = "bookclub_id")
     private BookClub bookClub;
 
+    private Boolean joining;
+
     @Builder
-    public BookClubUser(Long id, User user, BookClub bookClub) {
+    public BookClubUser(Long id, User user, BookClub bookClub, Boolean joining) {
         this.id = id;
         this.user = user;
         this.bookClub = bookClub;
+        this.joining = joining;
+    }
+
+    public void setJoiningStatus(Boolean joining) {
+        this.joining = joining;
     }
 }
